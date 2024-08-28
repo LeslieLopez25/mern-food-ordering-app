d8c3bce7a01f23bb7ee76d465697f41c0e55f9e8
Leslie Lopez <leslie2515lopez@gmail.com>
2024-08-28 00:50:29 -0600

    Created user API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import myUserRoute from "../routes/MyUserRoute";
 import "dotenv/config";
 
 mongoose
@@ -11,9 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello!" });
-});
+app.use("/api/my/user", myUserRoute);
 
 const PORT = process.env.PORT || 7000;
 
