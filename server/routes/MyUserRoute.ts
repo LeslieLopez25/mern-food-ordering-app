b4cc350e68782fb082edf69e2eb4311187138482
Leslie Lopez <leslie2515lopez@gmail.com>
2024-09-03 01:02:24 -0600

    Adding more functionality to update user profile api

--- server/routes/MyUserRoute.ts
+++ server/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../src/middleware/auth";
+import { jwtCheck, jwtParse } from "../src/middleware/auth";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
 
 export default router;
