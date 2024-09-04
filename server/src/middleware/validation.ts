ec4add665429980dcfe0749489a572155b880f64
Leslie Lopez <leslie2515lopez@gmail.com>
2024-09-03 23:57:34 -0600

    Finished functionality for user profile api

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -1,4 +1,17 @@
-import { body } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 export const validateMyUserRequest = [
   body("name").isString().notEmpty().withMessage("Name must be a string"),
@@ -6,4 +19,7 @@ export const validateMyUserRequest = [
     .isString()
     .notEmpty()
     .withMessage("AddressLine1 must be a string"),
+  body("city").isString().notEmpty().withMessage("City must be a string"),
+  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  handleValidationErrors,
 ];
