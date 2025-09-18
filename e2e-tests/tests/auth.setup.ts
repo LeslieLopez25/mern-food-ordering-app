3a5f3b5873361a3ef019d051bc8a01234fa2bf92
Leslie Lopez <leslie2515lopez@gmail.com>
2025-09-18 00:56:35 -0600

    Created e2e-tests folder and added playwright

--- e2e-tests/tests/auth.setup.ts
+++ e2e-tests/tests/auth.setup.ts
@@ -1,23 +1,23 @@
-import { test as setup } from "@playwright/test";
+import { test } from "@playwright/test";
+import dotenv from "dotenv";
 import path from "path";
 
-const authFile = path.join(__dirname, "auth.json");
+dotenv.config({ path: path.resolve(__dirname, "../.env.e2e") });
 
+const authFile = path.resolve(__dirname, "storageState.json");
 const UI_URL = "http://localhost:5173";
 
-setup("authenticate", async ({ page }) => {
+test("authenticate", async ({ page }) => {
   await page.goto(UI_URL);
 
-  await page.click("text=Login");
-
-  await page.waitForURL("**/u/login");
+  await page.click("text=Log In");
+  await page.waitForSelector('input[name="username"]');
 
   await page.fill('input[name="username"]', process.env.TEST_AUTH0_EMAIL!);
   await page.fill('input[name="password"]', process.env.TEST_AUTH0_PASSWORD!);
 
   await page.click('button[type="submit"]');
-
-  await page.waitForURL(`{UI_URL/}**`);
+  await page.waitForURL(`${UI_URL}/**`);
 
   await page.context().storageState({ path: authFile });
 });
---
+++ e2e-tests/tests/order.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173";
+
+const TEST_AUTH0_EMAIL = process.env.TEST_AUTH0_EMAIL ?? "test@example.com";
+
+test("place an order flow", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.fill('[data-testid="search-bar"]', "Guadalajara");
+  await page.click('[data-testid="search-button"]');
+
+  await page.click("text=Sage & Salt");
+
+  await page.click("text=Menudo");
+  await page.click("text=Birria Tacos");
+  await page.click("text=Lasagna");
+
+  await page.click("text=Go to checkout");
+
+  await page.fill('input[name="name"]', "Test User");
+  await page.fill('input[name="addressLine1"]', "Test");
+  await page.fill('input[name="city"]', "Test");
+  await page.fill('input[name="country"]', "Test");
+  await page.click("text=Continue to payment");
+
+  await page.waitForURL("https://checkout.stripe.com/**");
+
+  await page.fill('input[placeholder="email"]', TEST_AUTH0_EMAIL);
+  await page
+    .frameLocator('iframe[name^="__privateStripeFrame"]')
+    .locator('[name="cardnumber"]')
+    .fill("4242424242424242");
+  await page
+    .frameLocator('iframe[name^="__privateStripeFrame"]')
+    .locator('input[name="exp-date"]')
+    .fill("12/34");
+  await page
+    .frameLocator('iframe[name^="__privateStripeFrame"]')
+    .locator('input[name="cvc"]')
+    .fill("123");
+
+  await page.fill('input[name="cardholder-name"]', "Test User");
+
+  await page.click("text=Pay");
+
+  await page.waitForURL("**/order-status");
+  await expect(page.getByText("Order Status")).toBeVisible();
+});
